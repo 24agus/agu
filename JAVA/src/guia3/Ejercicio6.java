8673b9dec2d7fdf6b1aaf78e038747e2266031c5
Agus <agus_244@hotmail.com.ar>
2023-03-22 16:41:27 -0300

    mensaje de confirmación

--- JAVA/src/guia3/Ejercicio6.java
+++ JAVA/src/guia3/Ejercicio6.java
@@ -13,43 +13,55 @@ import java.util.Scanner;
  */
 public class Ejercicio6 {
       public static void main(String[] args) {
-        // TODO code application logic here
         Scanner leer = new Scanner(System.in);
-          System.out.println("ingrese un numero");
-          int num1=leer.nextInt();
-          
-          System.out.println("ingrese otro numero");
-          int num2=leer.nextInt();
+        System.out.println("Ingrese el primer numero entero con el que desea trabajar");
+        int num1 = leer.nextInt();
+        System.out.println("Ingrese el segundo numero entero con el que desea trabajar");
+        int num2 = leer.nextInt();
+        int eleccion = 0; //lo tuve que definir afuera del bucle sino me daba error
+        do{
+        System.out.println("--ELIJA UNA OPCION");
+        System.out.println("---MENU---");
+        System.out.println("1. SUMAR");
+        System.out.println("2. RESTAR");
+        System.out.println("3. MULTIPLICAR");
+        System.out.println("4. DIVIDIR");
+        System.out.println("5. SALIR");
         
-          int resultado;
+        eleccion = leer.nextInt();
         
-        System.out.println("ingrese una opcion");
-        
-        int opcion = leer.nextInt();
-        
-        switch (opcion) {
+        switch (eleccion){
+            
             case 1:
-                resultado=num1+num2;
-                System.out.println("la suma es " + resultado);
+                System.out.println("El resultado de la suma de los numeros ingresados es " + (num1+num2));
                 break;
             case 2:
-                resultado=num1-num2;
-                System.out.println("la resta es " + resultado);
+                System.out.println("El resultado de la resta de los numeros ingresados es " + (num1-num2));
                 break;
             case 3:
-                resultado=num1*num2;
-                System.out.println("la multi es " + resultado);
+                System.out.println("El resultado de la multiplicacion de los numeros ingresados es " + (num1*num2));
                 break;
             case 4:
-                resultado=num1/num2;
-                System.out.println("la division es " + resultado);
+                System.out.println("El resultado de la division de los numeros ingresados es " + (num1/num2));
+                break;
             case 5:
-             
-                System.out.println("salir");
-            default:
-                System.out.println("salir");
-                   
-    
+                    System.out.print("¿Está seguro que desea salir del programa (S/N)? ");
+                    String confirmacion = leer.next();
+                    
+                    if ("s".equals(confirmacion) || "S".equals(confirmacion)){
+                        System.out.println("Saliendo del programa...");
+                    } else {
+                        eleccion = 0;
+                    }
+                    break;
+            default: 
+                   System.out.println("Opción inválida. Por favor, elija una opción del 1 al 5.");
+                    break;
+        }
+        } while (eleccion != 5);
+        
+    }
 }
-}
-}
+
+
+
